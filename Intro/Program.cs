c16ed2ebf7a986ba22d471f435ea1a341a988ff0
Yigit Yalcin <yigityalciin@hotmail.com>
2024-03-10 13:00:56 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0;i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+courseManager.GetAll();
+
+BaseCustomer[] customers = {}
