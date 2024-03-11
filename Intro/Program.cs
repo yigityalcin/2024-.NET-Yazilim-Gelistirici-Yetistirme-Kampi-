a503ace11f7eeab4b1db75091d2f51506f2ede7f
Yigit Yalcin <yigityalciin@hotmail.com>
2024-03-11 03:58:47 +0300

    Lesson 2 Commit 2

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -10,4 +10,37 @@ for (int i = 0;i < courses2.Length; i++)
 }
 courseManager.GetAll();
 
-BaseCustomer[] customers = {}
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678922";
+customer1.FirstName = "Aslı";
+customer1.LastName = "Karayiğit";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "56478965412";
+customer2.FirstName = "Özgür";
+customer2.LastName = "Atılgan";
+customer2.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlamaio";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "12345678987";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "Abc";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "12345678985";
+
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+
+//Polymorphism 
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine(customer.Id);
+}
