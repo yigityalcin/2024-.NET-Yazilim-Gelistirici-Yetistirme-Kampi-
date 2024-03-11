ba4b111ee213c8ff68ec1936c5518b62e4886b2b
Yigit Yalcin <yigityalciin@hotmail.com>
2024-03-11 21:51:43 +0300

    Lesson 2 Finish

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Intro.Business;
+using Intro.DataAccess.Concretes;
 using Intro.Entities;
 
 
-CourseManager courseManager = new();
-Course[] courses2 = courseManager.GetAll();
-for (int i = 0;i < courses2.Length; i++)
+CourseManager courseManager = new(new CourseDal());
+List<Course> courses2 = courseManager.GetAll();
+for (int i = 0;i < courses2.Count; i++)
 {
     Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
 }
